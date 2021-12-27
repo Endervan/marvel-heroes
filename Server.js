6dcf68cd8a128adc43f190cf58ef2f52bfed9c33
edvan.ximenes <endvan@gmail.com>
2021-12-27 14:05:08 -0300

    server.js reajust

--- Server.js
+++
@@ -1,6 +0,0 @@
-import * as app from "blocking-proxy";
-
-const port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function () {
-  console.log("Listening on Port 3000");
-});
---
+++ server.js
@@ -0,0 +1,5 @@
+import * as app from "blocking-proxy";
+
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0'
+app.listen(port, host, ()=> console.log(`server is running on port ${port}`)
